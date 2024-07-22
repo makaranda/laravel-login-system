50e54216861f6fdba89469fa5cb25f08c9aeb8f7
Makaranda <makarandapathirana@gmail.com>
2024-07-22 08:57:43 +0530

    updated

--- app/Http/Controllers/dashboard/UsersController.php
+++ app/Http/Controllers/dashboard/UsersController.php
@@ -84,7 +84,6 @@ class UsersController extends Controller
                         return redirect()->Back()->withInput()->withErrors($validator);
                     } else {
 
-
                         $userData = [
                             'username' => $request->username,
                             'email' => $request->email,
